b873e50a04dd9be59743d2d6c29a261fc997ace0
waka421 <wakatabe421@gmail.com>
2024-07-09 16:10:20 +0900

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,55 @@
+const heading = document.querySelector('#my');
+
+const keyframes = {
+  opacity: [0,1],
+  translate: ['0 50px', 0]
+};
+const options = {
+  duration: 2000,
+  easing: 'ease',
+};
+
+heading.animate(keyframes, options);
+
+const menuOpen = document.querySelector('#menu-open');
+const menuClose = document.querySelector('#menu-close');
+const menuPanel = document.querySelector('#menu-panel');
+const menuOptions = {
+  duration: 300,
+  easing: 'ease',
+  fill: 'forwards',
+};
+
+menuOpen.addEventListener('click', () => {
+  menuPanel.animate({translate: ['100vw',0]}, menuOptions);
+});
+
+menuClose.addEventListener('click', () => {
+  menuPanel.animate({translate: [0, '100vw']}, menuOptions);
+});
+
+const animateFade = (entries, obs) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.animate(
+        {
+          opacity: [0,1],
+          translate: ['0 4rem', 0],
+        },
+        {
+          duration: 2000,
+          easing: 'ease',
+          fill: 'forwards'
+        }
+      );
+      obs.unobserve(entry.target);
+    }
+  });
+}
+
+const fadeObserver = new IntersectionObserver(animateFade);
+
+const fadeElements = document.querySelectorAll('.fadein');
+fadeElements.forEach((fadeElement) => {
+  fadeObserver.observe(fadeElement);
+});
